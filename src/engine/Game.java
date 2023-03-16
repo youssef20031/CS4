1a1a885ae03f3ca825fe76b09358367bc814cd46
youssefmaged <youssef.maged.6000@gmail.com>
2023-03-16 15:29:02 +0200

    implementing classes and interfaces

---
+++ src/engine/Game.java
@@ -0,0 +1,15 @@
+package engine;
+
+import model.characters.Hero;
+import model.characters.Zombie;
+import model.world.Cell;
+
+import java.util.ArrayList;
+
+public class Game {
+    public static ArrayList<Hero> availableHeros;
+    public static ArrayList<Hero> heros;
+    public static ArrayList<Zombie> zombies;
+    public static Cell[][] map;
+
+}
